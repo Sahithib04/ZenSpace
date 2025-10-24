fd815d2fa9284bc05fa2ccf45810ff348431e662
Bethi sahithi <bethisahithireddy044@gmail.com>
2025-10-24 13:09:01 +0530

    Initial commit

---
+++ src/main/java/com/app/zenzone/repository/JournalRepository.java
@@ -0,0 +1,16 @@
+package com.app.zenzone.repository;
+
+import com.app.zenzone.model.Journal;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface JournalRepository extends JpaRepository<Journal, Long> {
+
+
+   
+}
